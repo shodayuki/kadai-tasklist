c6e502b6a9e5ffa1fa882519d69e19407fda13d2
yuukishouda <yshoda1227@gmail.com>
2019-09-14 02:06:08 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,16 @@
 @section('content')
     <div class="center jumbotron">
         <div class="text-center">
-            <h1>Welcome to the Taskllist</h1>
-            {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+            @if (Auth::check())
+                {{ Auth::user()->name }}
+            @else
+                <div class="center jumbotron">
+                    <div class="text-center">
+                        <h1>Welcome to the Tasklist</h1>
+                        {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+                    </div>
+                </div>
+            @endif
         </div>
     </div>
 @endsection
